bcc3487f4c2fa3082feba813970b1bbee60181b7
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2021-12-01 01:45:49 +0530

    feat: added high scores

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ var userName = readlineSync.question("Please enter your username: ")
 console.log("Welcome to 'How Well Do You Know Me?', ", userName, "!")
 score = 0
 
+var highScores = ["Sinchana: 4", "Sahana: 3"]
 
 var questionOne = {
-question: "What is my age? ",
-answer: "23"
+question: "What is my favourite book? ",
+answer: "harry potter"
 }
 
 var questionTwo = {
-question: "Where do I live? ",
-answer: "Mysore"
+question: "What is my favourite pet? ",
+answer: "dog"
 }
 
 
 var questionThree = {
-question: "Where do I work? ",
-answer: "Infosys"
+question: "How many siblings do I have? ",
+answer: "1"
 }
 
-var questions = [questionOne, questionTwo, questionThree]
+var questionFour = {
+    question: "What instrument do I play? ",
+    answer: "ukulele"
+  }
+
+var questions = [questionOne, questionTwo, questionThree, questionFour]
 
 for (i=0; i<questions.length; i++){
 userAnswer =readlineSync.question(questions[i].question)
@@ -39,3 +45,5 @@ console.log("---------------")
 
 console.log ("You have reached the end of the game.")
 console.log ("Thank you for playing! Your total score is: ", score)
+console.log("Here are the high scores: ", highScores)
+console.log("If you have hit one of these two scores, send me a screenshot and I'll update it!")
